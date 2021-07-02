843e48f8eaf13a9833347f129a8b03d9099533e5
Brett Stevenson <bstevensondev@gmail.com>
2021-07-01 20:37:36 -0700

    Transition from sqlite to postgres

--- awesomelink/settings/base.py
+++ awesomelink/settings/base.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'awesomelink.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'awesomelink_db',
+        'USER': 'admin',
+        'PASSWORD' : 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
